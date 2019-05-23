15d7550dc7fbdf6705f11a98b0a044309aa73cef
Plane-walker <a990990@163.com>
2019-05-23 23:32:52 +0800

    udate

--- sms/src/filter/loginfilter.java
+++ sms/src/filter/loginfilter.java
@@ -7,12 +7,11 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
-import account.account;
-
+@WebFilter("/loginfilter")
 public class loginfilter implements Filter{
 
 	@Override
--- sms/src/servlet/refreshserv.java
+++ sms/src/servlet/refreshserv.java
@@ -6,9 +6,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import account.account;
 import stock.stock;
 
 /**
