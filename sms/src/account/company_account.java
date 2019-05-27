cdbc4eeaa077b93e58f8f421271f54ec75897eee
stones0205 <1525100446@qq.com>
2019-05-27 21:16:37 +0800

    update publish function

--- sms/src/account/company_account.java
+++ sms/src/account/company_account.java
@@ -12,19 +12,18 @@ public class company_account extends account{
 	@Override
 	public String register(HttpServletRequest request) {
 		String error=null;
-		HttpSession session = req.getSession();
-		String ID=session.getAttribute("ID");
-		String name=request.getParameter("username");
+		String userID=request.getParameter("userID");
+		String username=request.getParameter("username");
         String country=request.getParameter("country");
 		String pwHash=getmd5(request.getParameter("password"));
 		if(ID == null || ID.trim().equals(""))
-			error="用户ID不能为空！";
+			error="鐢ㄦ埛ID涓嶈兘涓虹┖锛�";
 		else if (name == null || name.trim().equals("")) 
-			error="用户名不能为空！";
-		else if (!request.getParameter("password").matches("^(?!.*[\\u4e00-\\u9fa5])(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{5,18}$"))
-				error="密码必须为5-18位，且必须同时包含大小写字母和数字！";
+			error="鐢ㄦ埛鍚嶄笉鑳戒负绌猴紒";
+		else if (!request.getParameter("password").matches("[^\\u3400-\\u9FFF]{5,18}"))
+				error="瀵嗙爜蹇呴』涓�5-18浣嶏紝涓斿繀椤诲悓鏃跺寘鍚ぇ灏忓啓瀛楁瘝鍜屾暟瀛楋紒";
 		else if (!request.getParameter("repassword").equals(request.getParameter("password"))) 
-				error="两次密码不一致！";
+				error="涓ゆ瀵嗙爜涓嶄竴鑷达紒";
 		else {
 			dbconnect dc=null;
 			PreparedStatement psta=null;
@@ -32,30 +31,57 @@ public class company_account extends account{
 			try{
 				dc=new dbconnect();
 				psta=dc.getconn().prepareStatement(
-						"select ID from individual_account"
-						+ " where ID=? and pwHash=?");
+						"select ID from company_account"
+						+ " where ID=?");
 				psta.setString(1, ID);
-				psta.setString(2, pwHash);
 				rs=dc.query(psta);
 				if(!rs.next()) {
 				psta=dc.getconn().prepareStatement(
-						"insert into company_account(ID,name,pwHash,country)"
+						"insert into company_account(ID,company_name,pwHash,country)"
 						+ " value(?,?,?,?) ");
-				psta.setString(1, ID);
-				psta.setString(2, name);
+				psta.setString(1, userID);
+				psta.setString(2, username);
 				psta.setString(3, pwHash);
 				psta.setString(4, country);
 				dc.add(psta);
-				this.ID=ID;
-				this.name=name;
+				ID=userID;
+				name=username;
 				}
 				else {
-					error="用户名已存在！";
+					error="鐢ㄦ埛鍚嶅凡瀛樺湪锛�";
 				}
 			}catch(Exception e) {
 				e.printStackTrace();
 			}
 			}
+		return error;
+	}
+
+	@Override
+	public String login(HttpServletRequest request) {
+		String error=null;
+		String userID=request.getParameter("userID");
+		String pwHash=getmd5(request.getParameter("password"));
+		dbconnect dc=null;
+		PreparedStatement psta=null;
+		ResultSet rs = null;
+		try{
+			dc=new dbconnect();
+			psta=dc.getconn().prepareStatement(
+					"select ID,company_name from company_account"
+					+ " where ID=? and pwHash=?");
+			psta.setString(1, userID);
+			psta.setString(2, pwHash);
+			rs=dc.query(psta);
+			if(rs.next()) {
+				this.ID=rs.getString("ID");
+				this.name=rs.getString("name");
+			}
+			else
+				error="鐢ㄦ埛鍚嶆垨瀵嗙爜閿欒";
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
 		return error;
 	}
 	public String publish(HttpServletRequest request)
